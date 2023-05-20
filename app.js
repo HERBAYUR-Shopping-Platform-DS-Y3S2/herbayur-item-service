d1fc0225ac290b9da54fd0db95a45628e53bc2d8
IT20653118 <it20653118@my.sliit.lk>
2023-05-20 10:48:29 +0530

    image multer added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const path = require("path");
+const multer = require('multer');
 
 // routes
 const items = require("./routes/items"); //use this when implementing routes
@@ -25,6 +26,32 @@ app.use("/api/items", items); //use this when implementing routes
 app.use("/api/user", user); //use this when implementing routes
 
 app.use(express.json({ extended: false }));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+const update = multer({ storage: storage });
+app.put("/update", update.single("file"), (req, res) => {
+  res.status(200).json("File has been updated");
+});
+
+app.use(cors())
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+app.post('/image', upload.single('file'), function (req, res) {
+  res.json({})
+})
 
 const PORT = process.env.PORT || 5003;
 
